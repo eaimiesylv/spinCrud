7bb3e0e8a79a02707c58ae0a8eb1e0ca0efa51e9
Emmanuel Okum <emmanuel.okum@idea2solutions.com>
2024-01-31 08:43:19 +0100

    Added user authentication

--- api/routes/api.php
+++ api/routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return 'ok';
+//    // return $request->user();
+// });
+
+
+Route::middleware('auth:sanctum')->group(function() {
+    route::get('/user',function(){
+        return 'route is protected ';
+    });
+  
+   
 });
