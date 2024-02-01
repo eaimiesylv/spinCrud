aab8fa270946108a1513c5870becbdb0179028ac
Emmanuel Okum <emmanuel.okum@idea2solutions.com>
2024-02-01 04:15:23 +0100

    created tasks can be updated

--- api/app/Services/TaskService/TaskRepository.php
+++ api/app/Services/TaskService/TaskRepository.php
@@ -3,6 +3,7 @@
 namespace App\Services\TaskService;
 use App\Models\Task;
 use Illuminate\Database\QueryException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 
 
@@ -25,9 +26,12 @@ class TaskRepository
     public function updateTask(array $request, $task)
     {
         try{
-            $update = $task->update($request);
+
+             $task->update($request);
+             return $task;
         }
-        catch (ModelNotFoundException $e) {
+       catch (QueryException $exception){
+        
             return response()->json(['message' => 'Task not found.'], 404);
         }
       
