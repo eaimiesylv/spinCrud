b86caff1e1850291833159e406efd8d3b8c17ec7
Emmanuel Okum <emmanuel.okum@idea2solutions.com>
2024-02-02 22:37:16 +0100

    removed password confirmation field

--- api/app/Http/Requests/UserFormRequest.php
+++ api/app/Http/Requests/UserFormRequest.php
@@ -15,7 +15,7 @@ class UserFormRequest extends FormRequest
         return [
             'email'=>'required|email|max:55|unique:users',
             'name' => 'required|string|max:55',
-            'password' => 'required|string|confirmed|min:2|max:60'
+            'password' => 'required|string|min:2|max:60'
         
         ];
     }
